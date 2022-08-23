970bff3bcf9197a5720b2b6d6c74f9f19d0fed58
mamun-prodhan <mamunpr9@gmail.com>
2022-08-23 15:07:03 +0600

    player list creation completed

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@
 const playerArray = [];
 
 function display(selectedTotalPlayer) {
-
     const playerList = document.getElementById('football-players');
     playerList.innerHTML = '';
 
     for (let i = 0; i < selectedTotalPlayer.length; i++) {
-        // console.log(selectedTotalPlayer[i]);
         const playerName = selectedTotalPlayer[i];
-        const listItem = document.createElement('li');
-        listItem.innerText = playerName;
-        playerList.appendChild(listItem);
+        const playerListItem = document.createElement('li');
+        playerListItem.innerText = playerName;
+        playerList.appendChild(playerListItem);
     }
 }
 
@@ -19,16 +17,11 @@ function selectPlayer(element) {
     const selectPlayerName = element.parentNode.parentNode.children[0].innerText;
     if (playerArray.length + 1 <= 5) {
         playerArray.push(selectPlayerName);
-        element.disabled = true;
-
         document.getElementById('selected-player-number').innerText = playerArray.length;
-        console.log(playerArray);
+        element.disabled = true;
         display(playerArray);
     }
     else {
         alert('already selected 5 players');
     }
-
-
-
 }
